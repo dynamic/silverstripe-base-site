cb1e8bcec20de448c391f9a7deb87c8b2acac4e8
mak001 <mak001001@gmail.com>
2018-01-03 13:06:38 -0600

    Added titles to areas in the cms

--- src/ElementPage.php
+++ src/ElementPage.php
@@ -6,6 +6,7 @@ use DNADesign\Elemental\Models\ElementalArea;
 use SilverStripe\Security\Member;
 use SilverStripe\Security\Permission;
 use SilverStripe\Security\PermissionProvider;
+use SilverStripe\Forms\FieldList;
 
 class ElementPage extends \Page implements PermissionProvider
 {
@@ -37,6 +38,16 @@ class ElementPage extends \Page implements PermissionProvider
     private static $owns = [
         'ElementalSidebar'
     ];
+
+    /**
+     * @return FieldList
+     */
+    public function getCMSFields() {
+        $fields = parent::getCMSFields();
+        $fields->dataFieldByName('ElementalSidebar')->setTitle('Sidebar');
+        $fields->dataFieldByName('ElementalArea')->setTitle('Main');
+        return $fields;
+    }
 
     /**
      * @param null|Member $member
