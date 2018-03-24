51bae69571a44df7ab0053999c22eb5d0134452e
Jason Irish <jsirish@users.noreply.github.com>
2018-03-24 15:33:17 -0500

    CampaignLandingPage - permissions, tests (#36)

--- tests/Page/CampaignLandingPageTest.php
+++ tests/Page/CampaignLandingPageTest.php
@@ -5,6 +5,7 @@ namespace Dynamic\Base\Test;
 use Dynamic\Base\Page\CampaignLandingPage;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Security\Member;
 
 class CampaignLandingPageTest extends SapphireTest
 {
@@ -21,5 +22,82 @@ class CampaignLandingPageTest extends SapphireTest
         $object = $this->objFromFixture(CampaignLandingPage::class, 'spring');
         $fields = $object->getCMSFields();
         $this->assertInstanceOf(FieldList::class, $fields);
+    }
+
+    /**
+     *
+     */
+    public function testCanView()
+    {
+        $object = $this->objFromFixture(CampaignLandingPage::class, 'spring');
+
+        $admin = $this->objFromFixture(Member::class, 'admin');
+        $this->assertTrue($object->canView($admin));
+
+        $siteowner = $this->objFromFixture(Member::class, 'site-owner');
+        $this->assertTrue($object->canView($siteowner));
+
+        $member = $this->objFromFixture(Member::class, 'default');
+        $this->assertFalse($object->canView($member));
+    }
+
+    /**
+     *
+     */
+    public function testCanEdit()
+    {
+        $object = $this->objFromFixture(CampaignLandingPage::class, 'spring');
+
+        $admin = $this->objFromFixture(Member::class, 'admin');
+        $this->assertTrue($object->canEdit($admin));
+
+        $siteowner = $this->objFromFixture(Member::class, 'site-owner');
+        $this->assertTrue($object->canEdit($siteowner));
+
+        $member = $this->objFromFixture(Member::class, 'default');
+        $this->assertFalse($object->canEdit($member));
+    }
+
+    /**
+     *
+     */
+    public function testCanDelete()
+    {
+        $object = $this->objFromFixture(CampaignLandingPage::class, 'spring');
+
+        $admin = $this->objFromFixture(Member::class, 'admin');
+        $this->assertTrue($object->canDelete($admin));
+
+        $siteowner = $this->objFromFixture(Member::class, 'site-owner');
+        $this->assertTrue($object->canDelete($siteowner));
+
+        $member = $this->objFromFixture(Member::class, 'default');
+        $this->assertFalse($object->canDelete($member));
+    }
+
+    /**
+     *
+     */
+    public function testCanCreate()
+    {
+        $object = $this->objFromFixture(CampaignLandingPage::class, 'spring');
+
+        $admin = $this->objFromFixture(Member::class, 'admin');
+        $this->assertTrue($object->canCreate($admin));
+
+        $siteowner = $this->objFromFixture(Member::class, 'site-owner');
+        $this->assertTrue($object->canCreate($siteowner));
+
+        $member = $this->objFromFixture(Member::class, 'default');
+        $this->assertFalse($object->canCreate($member));
+    }
+
+    /**
+     *
+     */
+    public function testProvidePermissions()
+    {
+        $object = $this->objFromFixture(CampaignLandingPage::class, 'spring');
+        $this->assertTrue(is_array($object->providePermissions()));
     }
 }
