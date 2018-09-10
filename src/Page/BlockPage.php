4014ca592c37cfed5b20384b504f6f897025a576
Jason Irish <jirish@dynamicagency.com>
2018-09-10 16:00:58 -0500

    refactor - remove default config
    move to ReadMe

---
+++ src/Page/BlockPage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dynamic\Base\Page;
+
+class BlockPage extends \Page
+{
+    /**
+     * @var string
+     */
+    private static $singular_name = 'Block Page';
+
+    /**
+     * @var string
+     */
+    private static $plural_name = 'Block Pages';
+
+    /**
+     * @var string
+     */
+    private static $description = 'Flexible layout page created using content blocks';
+}
---
+++ src/Page/BlockPageController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Dynamic\Base\Page;
+
+class BlockPageController extends \PageController
+{
+    
+}
