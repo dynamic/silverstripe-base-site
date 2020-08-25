999969165fc074b688c4fac397d21a995a21a439
Jason Irish <jirish@dynamicagency.com>
2020-08-25 13:15:16 -0500

    REFACTOR SeoExtension - use existing SEO fields

--- src/Extension/SeoExtension.php
+++ src/Extension/SeoExtension.php
@@ -2,7 +2,10 @@
 
 namespace Dynamic\Base\Extension;
 
+use Axllent\CMSTweaks\MetadataTab;
+use QuinnInteractive\Seo\Extensions\PageSeoExtension;
 use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Core\Config\Config;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextareaField;
@@ -43,75 +46,41 @@ class SeoExtension extends DataExtension
      */
     public function updateCMSFields(FieldList $fields)
     {
-        parent::updateCMSFields($fields);
-
-        if ($this->owner instanceof \SilverStripe\ErrorPage\ErrorPage) {
-            return;
+        if (class_exists(MetadataTab::class)) {
+            $config = Config::inst();
+            $tab_title = $config->get(MetadataTab::class, 'tab_title');
+        } else {
+            $tab_title = 'SEO';
         }
 
-        $fields->removeByName([
-            'SEOHealthAnalysis'
-        ]);
+        if (class_exists(PageHealthExtension::class)) {
+            $pagehealth = $fields->fieldByName('Root.Main.SEOHealthAnalysis');
+            $pagehealth_fields = $pagehealth->FieldList();
+            $fields->removeFieldFromTab('Root.Main', 'SEOHealthAnalysis');
 
-        $fields->addFieldsToTab('Root.SEO', [
-            ToggleCompositeField::create('SEOHealthAnalysis', 'SEO Health Analysis', [
-                GoogleSearchPreview::create(
-                    'GoogleSearchPreview',
-                    'Search Preview',
-                    $this->getOwner(),
-                    $this->owner->getRenderedHtmlDomParser()
-                ),
-                TextField::create('FocusKeyword', 'Set focus keyword'),
-                HealthAnalysisField::create('ContentAnalysis', 'Content Analysis', $this->getOwner()),
-            ])
-        ]);
+            $fields->addFieldsToTab(
+                'Root.' . $tab_title,
+                $pagehealth
+            );
+        }
 
-        $fields->removeByName([
-            'FacebookSeoComposite',
-            'TwitterSeoComposite',
-        ]);
+        if (class_exists(PageSeoExtension::class)) {
+            $facebook = $fields->fieldByName('Root.Main.FacebookSeoComposite');
+            $fields->removeFieldFromTab('Root.Main', 'FacebookSeoComposite');
 
-        $fields->addFieldsToTab('Root.SEO', [
-            ToggleCompositeField::create('FacebookSeoComposite', 'Facebook SEO', [
-                DropdownField::create('FacebookPageType', 'Type', FacebookMetaGenerator::getValidTypes()),
-                TextField::create('FacebookPageTitle', 'Title')
-                    ->setAttribute('placeholder', $this->getOwner()->Title)
-                    ->setRightTitle('If blank, inherits default page title')
-                    ->setTargetLength(45, 25, 70),
-                UploadField::create('FacebookPageImage', 'Image')
-                    ->setRightTitle('Facebook recommends images to be 1200 x 630 pixels. If no image is
-                        provided, facebook will choose the first image that appears on the page which usually has
-                        bad results')
-                    ->setFolderName('seo'),
-                TextareaField::create('FacebookPageDescription', 'Description')
-                    ->setAttribute(
-                        'placeholder',
-                        $this->getOwner()->MetaDescription ?:
-                        $this->getOwner()->dbObject('Content')->LimitCharacters(297)
-                    )
-                    ->setRightTitle('If blank, inherits meta description if it exists or gets the first 297
-                        characters from content')
-                    ->setTargetLength(200, 160, 320),
-            ]),
-            ToggleCompositeField::create('TwitterSeoComposite', 'Twitter SEO', [
-                TextField::create('TwitterPageTitle', 'Title')
-                    ->setAttribute('placeholder', $this->getOwner()->Title)
-                    ->setRightTitle('If blank, inherits default page title')
-                    ->setTargetLength(45, 25, 70),
-                UploadField::create('TwitterPageImage', 'Image')
-                    ->setRightTitle('Must be at least 280x150 pixels')
-                    ->setFolderName('seo'),
-                TextareaField::create('TwitterPageDescription', 'Description')
-                    ->setAttribute(
-                        'placeholder',
-                        $this->getOwner()->MetaDescription ?:
-                        $this->getOwner()->dbObject('Content')->LimitCharacters(297)
-                    )
-                    ->setRightTitle('If blank, inherits meta description if it exists or gets the first 297
-                        characters from content')
-                    ->setTargetLength(200, 160, 320),
-            ])
-        ]);
+            $fields->addFieldsToTab(
+                'Root.' . $tab_title,
+                $facebook
+            );
+
+            $twitter = $fields->fieldByName('Root.Main.TwitterSeoComposite');
+            $fields->removeFieldFromTab('Root.Main', 'TwitterSeoComposite');
+
+            $fields->addFieldsToTab(
+                'Root.' . $tab_title,
+                $twitter
+            );
+        }
     }
 
     /**
@@ -131,7 +100,7 @@ class SeoExtension extends DataExtension
     {
         parent::onBeforeWrite();
 
-        // set Content to output of blocks for search
+        // set SearchContent to output of blocks for search
         if ($this->owner->hasMethod('getElementsForSearch')) {
             $this->owner->SearchContent =
                 ltrim(
